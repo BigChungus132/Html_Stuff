4be274b476e9f12596571387bebaebdec9afca50
Jackson Crews <jcrews21@pusd.pcoe.k12.ca.us>
2019-04-25 14:49:18 -0700

    inital commit

---
+++ sketch.js
@@ -0,0 +1,17 @@
+let can;
+
+function setup() {
+can=createCanvas(700,500);can.position(30,25);
+can.class('bdd');
+tChoice=new logo(200,100,0);
+textSize(20);
+
+tv=new logo(200,200,0);
+
+}
+
+function draw() {
+//clear();
+background(200,160,120);
+chgRec();
+
